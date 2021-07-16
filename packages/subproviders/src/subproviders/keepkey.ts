d962e5ad596dc67cc3bd069f6d6d98d07355f28f
highlander <bithighlander@gmail.com>
2021-07-16 13:56:54 -0600

    init

---
+++ packages/subproviders/src/subproviders/keepkey.ts
@@ -0,0 +1,7 @@
+import { assert } from '@0x/assert';
+import { addressUtils } from '@0x/utils';
+import EthereumTx = require('ethereumjs-tx');
+import * as _ from 'lodash';
+
+import HDNode = require('hdkey');
+
